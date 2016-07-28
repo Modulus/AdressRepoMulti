0b2a46f440df487e6f1f31c81c78d4361284d3ad
Modulus <thejohnskauge@gmail.com>
2016-07-28 21:24:08 +0200

    Added sh file

---
+++ copy.sh
@@ -0,0 +1,16 @@
+
+cp -Rvu saltstack/salt/* /srv/salt
+
+
+cp -Rvu saltstack/pillar/* /srv/pillar
+
+
+cp -vu cloud.profiles.d/* /etc/salt/cloud.profiles.d/
+#cp -vu cloud.providers.d/* /etc/salt/cloud.providers.d/
+
+
+#Copy reactor code
+#cp -Rvu reactor/ /srv/
+
+
+#config should not be here, this is for vagrant setup
